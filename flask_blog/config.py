54c17c6e1682d5ba36c35e32e9cf441bf7c4a8ff
Karan9034 <karan.agr9034@gmail.com>
2020-11-03 11:37:05 +0530

    Hidden Stuff

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -2,8 +2,8 @@ import os
 
 
 class Config:
-    SECRET_KEY = '18256fdc199f95f0cdac2b6ddbae9214'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.office365.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
