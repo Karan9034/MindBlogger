dc3e4a38d65f77e15895940a65b152aa44556c12
Karan9034 <karan.agr9034@gmail.com>
2020-11-05 11:23:42 +0530

    Changes

--- flask_blog/config.py
+++ flask_blog/config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = "18256fdc199f95f0cdac2b6ddbae9214"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
     MAIL_SERVER = 'smtp.office365.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get('EMAIL_USER')
-    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+    MAIL_USERNAME = "mindblogger@hotmail.com"
+    MAIL_PASSWORD = "Karan$Admin@Blogger"
